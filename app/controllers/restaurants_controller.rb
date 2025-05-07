0f51df4212512076f01bf7aa52d0d64ca6ea8ff2
Daria Potopnic <daria.cnb@gmail.com>
2025-05-07 16:57:23 +0100

    Updated version Views using layouts and partials

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -2,4 +2,13 @@ class RestaurantsController < ApplicationController
   def index
     @restaurants = Restaurant.all
   end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
+    @review = Review.new
+  end
 end
